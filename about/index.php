8e5697d920f334c2de074d9e625279386cf5d57b
Ryan McCue <ryanmccue@cubegames.net>
2009-10-01 18:14:07 +1000

    Add link to GitHub and BSD license

--- about/index.php
+++ about/index.php
@@ -13,6 +13,7 @@ include(PATH . '/header.php');
 				<h1>About</h1>
 				<p>Design, and You! is a site written by <a href="http://ryanmccue.info/">Ryan McCue</a> for his IT assignment of 2009. It is intended to introduce CSS to site developers while also teaching them the basics of design theory.</p>
 				<p><del>Some</del> <ins>Most</ins> of the content is not yet finished, unfortunately, but it will be updated live at <a href="http://it.ryanmccue.info/">it.ryanmccue.info</a> and hopefully can become a resource for others to use.</p>
+				<p>The full source code for this site is available at <a href="http://github.com/rmccue/Design-and-You">github.com/rmccue/Design-and-You</a> and can be downloaded in either <a href="http://github.com/rmccue/Design-and-You/zipball/master">zip</a> or <a href="http://github.com/rmccue/Design-and-You/tarball/master">tarball</a> format. This code is available under <a href="../LICENSE">the BSD license</a>.</p>
 			</div>
 			<ul id="sidebar">
 				<li>
